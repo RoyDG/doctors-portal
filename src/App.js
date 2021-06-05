3ed85b4754bffc172e2072b7df05bfba21a8fff3
Roy <rdg01834@gmail.com>
2021-06-05 13:14:36 +0600

    router setup in app.js & home component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Pages/Home/Home';
+
 
 function App() {
   return (
-    <div className="App">
-     
-       
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
