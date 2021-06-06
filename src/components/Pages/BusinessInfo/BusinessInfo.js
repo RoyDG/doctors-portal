9880795df68670eaa71ae061912d0c0501d46dff
Roy <rdg01834@gmail.com>
2021-06-06 15:07:49 +0600

    BusinessInfo,InfoCard,FontAwesome,infoData

--- src/components/Pages/BusinessInfo/BusinessInfo.js
+++ src/components/Pages/BusinessInfo/BusinessInfo.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import InfoCard from '../InfoCard/InfoCard';
+import { faClock, faMapMarker, faPhone } from '@fortawesome/free-solid-svg-icons';
+const infoData = [
+    {
+        title: 'Opening Hours',
+        description: 'We are open 7 days',
+        icon: faClock,
+        background: 'Info'
+    },
+     {
+        title: 'Visit our location',
+        description: 'Brooklyn, NY 10003, USA',
+        icon: faMapMarker,
+        background: 'Secondary'
+    },
+     {
+        title: 'Call us now',
+        description: '+15698374652',
+        icon: faPhone,
+        background: 'Info'
+    }
+]
 
 const BusinessInfo = () => {
     return (
-        <div>
-            
-        </div>
+        <section className = "row">
+            {
+                infoData.map( info => <InfoCard info = {info}></InfoCard> )
+            }
+        </section>
     );
 };
 
